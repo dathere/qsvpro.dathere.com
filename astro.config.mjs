66de4f1fd05ca775fb08b31625a793957d95ffc5
rzmk <mueez25@gmail.com>
2023-12-05 11:38:58 -0500

    fix: use /qsv-pro-web as base path for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
+    site: "https://dathere.github.io",
+    base: "/qsv-pro-web",
+    integrations: [react(), tailwind()],
 });
